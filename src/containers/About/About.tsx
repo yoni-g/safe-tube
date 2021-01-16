92ce948a9ca97e9dd15ccf4af14b7598fe3a8e71
Yonathan Goriachnick <yonathan@200apps.co>
2021-01-16 23:39:39 +0200

    Added about container

---
+++ src/containers/About/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.scss';
+
+const About: React.FC = () => (
+  <div className="About" data-testid="About">
+    About Component
+  </div>
+);
+
+export default About;
