1337a5af6ae67d419f1b6614937353a700978b19
yoni-g <yonig770@gmail.com>
2021-01-16 23:49:01 +0200

    fixed scss version added nav bar comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import NavBar from './components/NavigationBar/NavigationBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+		<NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
