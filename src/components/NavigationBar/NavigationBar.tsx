d77a4649c4f20f4e1cffa2027731e7640ca749e7
Yonathan Goriachnick <yonathan@200apps.co>
2021-01-16 23:39:21 +0200

    added grc config added nav comp

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavigationBar.scss';
+
+const NavigationBar: React.FC = () => (
+  <div className="NavigationBar" data-testid="NavigationBar">
+    NavigationBar Component
+  </div>
+);
+
+export default NavigationBar;
